b8459dd677244e5936b0002c714c63e7d9a4e5b7
28JayG <jaygandhi.32@outlook.com>
2020-10-08 16:14:14 +0530

    updates contributers section and card UI

--- src/components/contributors/contributors.compnent.jsx
+++ src/components/contributors/contributors.compnent.jsx
@@ -1,18 +1,20 @@
 import React from 'react';
 import ContributorCard from '../contributor-card/contributor-card.componet';
 
-import './contributors.component.css'
+import './contributors.component.css';
 
 const Contributors = () => (
+  <div className='contributors-wrap'>
     <div className='contributors'>
-        <h3 className='title'>Contributors</h3>
-        <div className='cards-wrapper'>
-            <ContributorCard />
-            <ContributorCard />
-            <ContributorCard />
-            <ContributorCard />
-        </div>
+      <h3 className='title'>Contributors</h3>
+      <div className='cards-wrapper'>
+        <ContributorCard />
+        <ContributorCard />
+        <ContributorCard />
+        <ContributorCard />
+      </div>
     </div>
+  </div>
 );
 
 export default Contributors;
