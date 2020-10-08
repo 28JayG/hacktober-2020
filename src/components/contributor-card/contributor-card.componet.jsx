2d0c7171c8c0acc31da0e634b48b4e676ed3ef7c
28JayG <jaygandhi.32@outlook.com>
2020-10-08 07:53:22 +0530

    adds card

--- src/components/contributor-card/contributor-card.componet.jsx
+++ src/components/contributor-card/contributor-card.componet.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import faker from 'faker';
 
-const ContributorCard = () => {
-    return <div className='card'>
-        
-    </div>   
-}
+import { FaDocker, FaInstagram } from 'react-icons/fa';
 
+import './contributor-card.component.css';
+
+const ContributorCard = ({
+  name,
+  occupation,
+  socialMediaURLs,
+  featureImageURL,
+}) => {
+  return (
+    <div className='card'>
+      <img
+        src={faker.image.avatar()}
+        className='profile-image'
+        alt='profile photo'
+      />
+      <h6 className='name'>{faker.name.findName()}</h6>
+      <span className='occupation'>{'enterpenuere'}</span>
+      <div className='social-media-row'>
+        <FaInstagram size={32} color='white' />
+        <FaInstagram size={32} color='white' />
+        <FaInstagram size={32} color='white' />
+      </div>
+    </div>
+  );
+};
+
+export default ContributorCard;
