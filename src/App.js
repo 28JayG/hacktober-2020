efbe12972a5d7be756f2db0f5190a0821db7a2c8
28JayG <jaygandhi.32@outlook.com>
2020-10-08 07:02:43 +0530

    adds background to body * adds homepage header * adds contributors-section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+import HomePage from './pages/home/home.component';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
     <div>
-      
+      <Switch>
+        <Route to='/' exact component={HomePage} />
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
