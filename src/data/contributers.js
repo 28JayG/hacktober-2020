9df787e95de6149705a7edeb80eb37d4b4c898af
Biswajit Behera <suraj53028@gmail.com>
2020-10-18 14:28:54 +0530

    add contributers data

--- src/data/contributers.js
+++ src/data/contributers.js
@@ -1,33 +1,44 @@
-export const contributers={
-    '28JayG':{
-        name: 'Jay Gandhi',
-        occupation: 'Frontend Developer',
-        featureImageURL: 'https://i.ibb.co/prdFx6K/pp.jpg',
-        socialMediaURLs: {
-            instagram:'https://www.instagram.com/a.guy.in.corner/?hl=en',
-            github:'https://github.com/28JayG',
-            linkedin:'https://www.linkedin.com/in/jay-gandhi-92b720169/'
-        }
+export const contributers = {
+  "28JayG": {
+    name: "Jay Gandhi",
+    occupation: "Frontend Developer",
+    featureImageURL: "https://i.ibb.co/prdFx6K/pp.jpg",
+    socialMediaURLs: {
+      instagram: "https://www.instagram.com/a.guy.in.corner/?hl=en",
+      github: "https://github.com/28JayG",
+      linkedin: "https://www.linkedin.com/in/jay-gandhi-92b720169/",
     },
+  },
 
-    'Sumit1996-V':{
-        name: 'Sumit Vadodaria',
-        occupation: 'Business Analyst, Rupeek',
-        featureImageURL: 'https://i.ibb.co/t2jN3z8/pp-1.jpg',
-        socialMediaURLs: {
-            instagram:'https://www.instagram.com/sumit_vadodaria/?hl=en',
-            github:'https://github.com/Sumit1996-V/',
-            linkedin:'https://www.linkedin.com/in/sumit-vadodaria-9b7709123/'
-        }
+  "Sumit1996-V": {
+    name: "Sumit Vadodaria",
+    occupation: "Business Analyst, Rupeek",
+    featureImageURL: "https://i.ibb.co/t2jN3z8/pp-1.jpg",
+    socialMediaURLs: {
+      instagram: "https://www.instagram.com/sumit_vadodaria/?hl=en",
+      github: "https://github.com/Sumit1996-V/",
+      linkedin: "https://www.linkedin.com/in/sumit-vadodaria-9b7709123/",
     },
-     'Puja7629':{
-        name: 'Puja Kumari',
-        occupation: 'Data Science',
-        featureImageURL: 'https://i.ibb.co/48hBqfX/Whats-App-Image-2020-08-02-at-9-49-09-AM.jpg',
-        socialMediaURLs: {
-            instagram:'https://www.instagram.com/dynamic.rainbow/',
-            github:'https://github.com/Puja7629',
-            linkedin:'https://www.linkedin.com/in/puja7629/'
-        }
+  },
+  Puja7629: {
+    name: "Puja Kumari",
+    occupation: "Data Science",
+    featureImageURL:
+      "https://i.ibb.co/48hBqfX/Whats-App-Image-2020-08-02-at-9-49-09-AM.jpg",
+    socialMediaURLs: {
+      instagram: "https://www.instagram.com/dynamic.rainbow/",
+      github: "https://github.com/Puja7629",
+      linkedin: "https://www.linkedin.com/in/puja7629/",
     },
-}
+  },
+  biswa1751: {
+    name: "Biswajit Behera",
+    occupation: "Full stack Developer",
+    featureImageURL: "",
+    socialMediaURLs: {
+      instagram: "",
+      github: "",
+      linkedin: "",
+    },
+  },
+};
